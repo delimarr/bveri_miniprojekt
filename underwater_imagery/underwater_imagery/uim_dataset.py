4ae8050159545ad1cc9c729e7507870005301014
delimarr <robin.delimar@students.fhnw.ch>
2024-01-12 17:31:21 +0100

    dataset complete

--- underwater_imagery/underwater_imagery/uim_dataset.py
+++ underwater_imagery/underwater_imagery/uim_dataset.py
@@ -2,35 +2,42 @@ from pathlib import Path
 from typing import List, Tuple
 
 from PIL import Image
-from torch import Tensor
+from torch import Tensor, round, uint8, empty, all
 from torch.utils.data import Dataset
 from torchvision import io as t_io
 from torchvision import transforms
 
 
 class UIMDataset(Dataset):
-    def __init__(self, root_path: Path) -> None:
+    def __init__(self, root_path: Path, classes: List[Tuple[str, Tensor]]) -> None:
         self.root_path = root_path
-        self.train_path = root_path / "train_val"
-        self.test_path = root_path / "TEST"
 
-        self.classes = [
-            p.parts[-1] for p in self.test_path.glob("./masks/*") if p.is_dir()
-        ]
+        self.classes = classes
 
-        # build train set
-        self.train_images: List[Tensor] = []
-        self.train_masks: List[Tensor] = []
+        self.images: List[Tensor] = []
+        self.masks: List[Tensor] = []
+        self.label_images: List[Tensor] = []
 
         transform = transforms.ToTensor()
-        for image in self.train_path.glob("./images/*.jpg"):
-            self.train_images.append(t_io.read_image(str(image.absolute())))
-        for mask in self.train_path.glob("./masks/*.bmp"):
-            image = Image.open(str(mask.absolute()))
-            self.train_masks.append(transform(image))
+        for image in self.root_path.glob("./images/*.jpg"):
+            self.images.append(t_io.read_image(str(image.absolute())))
+        for label_image in self.root_path.glob("./masks/*.bmp"):
+            image = Image.open(str(label_image.absolute()))
+            image_tensor = transform(image)
+            self.label_images.append(round(image_tensor).to(uint8))
+
+            _, w, h = image_tensor.shape
+            mask = empty((len(self.classes), w, h), dtype=uint8)
+            for i, cls in enumerate(self.classes):
+                val = cls[1]
+                val = val.view(3, 1, 1)
+                mask[i] = all(image_tensor == val, dim=0).to(uint8)
+            self.masks.append(mask)
+
+        assert len(self.images) == len(self.label_images)
 
     def __len__(self) -> int:
-        return len(self.train_images)
+        return len(self.images)
 
-    def __getitem__(self, index: int) -> Tuple[Tensor, Tensor]:
-        return self.train_images[index], self.train_masks[index]
+    def __getitem__(self, index: int) -> Tuple[Tensor, Tensor, Tensor]:
+        return self.images[index], self.masks[index], self.label_images[index]
