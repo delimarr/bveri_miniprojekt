d149698f832dcb2a4b20544652637c70ff2a7d84
delimarr <robin.delimar@students.fhnw.ch>
2024-01-14 19:52:27 +0100

    metrics working

--- src/underwater_imagery/models/metrics.py
+++ src/underwater_imagery/models/metrics.py
@@ -1,18 +1,31 @@
 from torch import Tensor
 from torch import float as t_float
 from torchmetrics.classification import MulticlassConfusionMatrix, MulticlassRecall, MulticlassPrecision, MulticlassF1Score
+import torch.nn.functional as F
+import seaborn as sns
+import matplotlib.pyplot as plt
+from underwater_imagery.data.constants import CLASSES
 
-def mask_to_int_class(mask: Tensor) -> Tensor:
-    return mask.armgax(dim=(1), keepdim=True)
+def logits_to_idx_class(logits: Tensor) -> Tensor:
+    probs = F.softmax(logits, dim=1)
+    pred = probs.argmax(dim=(1), keepdim=True)
+    return pred.squeeze(1)
 
 def confusion_matrix(
     pred: Tensor, 
     true: Tensor, 
     num_classes: int = 8
 ) -> None:
+    pred = pred.to('cpu')
+    true = true.to('cpu')
     metric = MulticlassConfusionMatrix(num_classes=num_classes)
-    metric(pred, true)
-    metric.plot()
+    matrix = metric(pred, true)
+    matrix = matrix / matrix.sum() * 100
+    names = [classes[0] for classes in CLASSES]
+    sns.heatmap(matrix, annot=True, fmt='.2f', xticklabels=names, yticklabels=names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show(block=False)
 
 def recall(
     pred: Tensor, 
@@ -20,6 +33,8 @@ def recall(
     num_classes: int = 8,
     average: str = "micro"
 ) -> float:
+    pred = pred.to('cpu')
+    true = true.to('cpu')
     metric = MulticlassRecall(num_classes=num_classes, average=average)
     return metric(pred, true)
 
@@ -29,6 +44,8 @@ def precision(
     num_classes: int = 8,
     average: str = "micro"
 ) -> float:
+    pred = pred.to('cpu')
+    true = true.to('cpu')
     metric = MulticlassPrecision(num_classes=num_classes, average=average)
     return metric(pred, true)
 
@@ -38,6 +55,8 @@ def f1_score(
     num_classes: int = 8,
     average: str = "micro"
 ) -> float:
+    pred = pred.to('cpu')
+    true = true.to('cpu')
     metric = MulticlassF1Score(num_classes=num_classes, average=average)
     return metric(pred, true)
 
@@ -45,16 +64,20 @@ def mean_pixel_acc(
     pred: Tensor, 
     true: Tensor
 ) -> t_float:
+    pred = pred.to('cpu')
+    true = true.to('cpu')
     return (pred == true).to(t_float).mean()
 
 def iou(
     pred_masks: Tensor, 
     true_masks: Tensor, 
 ) -> t_float:
+    pred_masks = pred_masks.to('cpu')
+    true_masks = true_masks.to('cpu')
     pred_masks = pred_masks.to(bool)
     true_masks = true_masks.to(bool)
     intersection = (pred_masks == true_masks).sum()
-    union = (pred_masks| true_masks).sum()
+    union = (pred_masks | true_masks).sum()
     return intersection / union
 
 
