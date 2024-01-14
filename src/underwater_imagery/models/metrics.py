17b6b8dbfdf20f8e0eb7acfcf4e7204e6e3f0b78
delimarr <robin.delimar@students.fhnw.ch>
2024-01-14 20:38:36 +0100

    eval suim

--- src/underwater_imagery/models/metrics.py
+++ src/underwater_imagery/models/metrics.py
@@ -1,10 +1,13 @@
-from torch import Tensor
+from torch import Tensor, zeros
+from torch.utils.data import DataLoader
+from torch import nn
 from torch import float as t_float
 from torchmetrics.classification import MulticlassConfusionMatrix, MulticlassRecall, MulticlassPrecision, MulticlassF1Score
 import torch.nn.functional as F
 import seaborn as sns
 import matplotlib.pyplot as plt
 from underwater_imagery.data.constants import CLASSES
+from underwater_imagery.models.iou_loss import SoftIoULoss
 
 def logits_to_idx_class(logits: Tensor) -> Tensor:
     probs = F.softmax(logits, dim=1)
@@ -15,17 +18,13 @@ def confusion_matrix(
     pred: Tensor, 
     true: Tensor, 
     num_classes: int = 8
-) -> None:
+) -> Tensor:
     pred = pred.to('cpu')
     true = true.to('cpu')
     metric = MulticlassConfusionMatrix(num_classes=num_classes)
     matrix = metric(pred, true)
     matrix = matrix / matrix.sum() * 100
-    names = [classes[0] for classes in CLASSES]
-    sns.heatmap(matrix, annot=True, fmt='.2f', xticklabels=names, yticklabels=names)
-    plt.ylabel('Actual')
-    plt.xlabel('Predicted')
-    plt.show(block=False)
+    return matrix
 
 def recall(
     pred: Tensor, 
@@ -80,5 +79,55 @@ def iou(
     union = (pred_masks | true_masks).sum()
     return intersection / union
 
+def eval(
+    model: nn.Module, 
+    data_loader: DataLoader, 
+    num_classes: int = 8,
+    device: str = 'cuda',
+    max_iter: int = 3,
+) -> None:
+    precision_total = 0
+    recall_total = 0
+    f1_total = 0
+    mean_pixel_acc_total = 0
+    iou_loss_total = 0
+    entropy_loss_total = 0
+    matrix = zeros((num_classes, num_classes))
+    iou_loss = SoftIoULoss()
+    criterion = nn.CrossEntropyLoss()
+    for i, data in enumerate(data_loader):
+        images, label_masks, label_images = data
+        images: Tensor = images.to(device)
+        label_masks: Tensor = label_masks.to(device)
+        label_images: Tensor = label_images.to(device)
+
+        logits = model(images)
+        pred = logits_to_idx_class(logits)
+        true = logits_to_idx_class(label_masks)
+
+        precision_total += precision(pred, true)
+        recall_total += recall(pred, true)
+        f1_total += f1_score(pred, true)
+        mean_pixel_acc_total += mean_pixel_acc(pred, true)
+        iou_loss_total += iou_loss(logits, label_masks)
+        entropy_loss_total += criterion(logits, label_masks)
+        matrix += confusion_matrix(pred, true)
+        if i > max_iter:
+            break
+
+    i += 1
+    print("always micro")
+    print(f"precision: {precision_total / i:.3f}")
+    print(f"recall: {recall_total / i:.3f}")
+    print(f"f1 score: {f1_total / i:.3f}")
+    print(f"mean pixel acc: {mean_pixel_acc_total / i:.3f}")
+    print(f"soft log IoU loss: {iou_loss_total / i:.3f}")
+    print(f"cross entropy loss: {entropy_loss_total / i:.3f}")
+
+    names = [classes[0] for classes in CLASSES]
+    sns.heatmap(matrix / i, annot=True, fmt='.2f', xticklabels=names, yticklabels=names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show(block=False)
 
 
